63fbd890265d2b5e621579e2f58b21a1e9f9dd99
Andrew Radev <andrey.radev@gmail.com>
2024-07-19 19:06:11 +0200

    Working categorization (hopefully)

--- 02_db/snps.py
+++ 02_db/snps.py
@@ -6,5 +6,25 @@
 # 4. If snp is neither,         category = NULL
 #
 
-def categorize_snps():
-    pass
+def categorize_snps(db):
+    db.execute('''
+        UPDATE snp
+        SET category = 'Normal'
+        WHERE snp.snp in (
+            SELECT snp
+            FROM snp_call
+            WHERE sample = 'TLE66_N'
+            AND genotype_simple > 0
+        )
+    ''')
+
+    db.execute('''
+        UPDATE snp
+        SET category = IIF(category IS NULL, 'Tumor', 'Mixed')
+        WHERE snp.snp in (
+            SELECT snp
+            FROM snp_call
+            WHERE sample = 'TLE66_T'
+            AND genotype_simple > 0
+        )
+    ''')
--- 02_db/test_categorize_snps.py
+++ 02_db/test_categorize_snps.py
@@ -13,25 +13,103 @@ class TestCategorizeSnps(unittest.TestCase):
             self.db.execute(statement)
 
     def create_snp(self, snp_id):
-        pass
+        params = {
+            'snp': snp_id,
+            'chrom': 9,
+            'pos': 100_000,
+            'quality': 40,
+            'ref': 'A',
+            'alt': 'G',
+            'type': 'TODO',
+            'category': None,
+        }
+
+        self.db.execute("""
+            INSERT INTO snp VALUES (
+                :snp, :chrom, :pos, :quality, :ref, :type, :alt, :category
+            )
+        """, params)
 
     def create_snp_call(self, snp_id, **params):
-        pass
+        default_params = {
+            'snp': snp_id,
+            'sample': 'TLE66_N',
+            'genotype': '1/1',
+            'genotype_simple': 2,
+        }
+
+        params = {**default_params, **params}
+
+        self.db.execute("""
+            INSERT INTO snp_call VALUES (
+                :snp, :sample, :genotype, :genotype_simple
+            )
+        """, params)
 
     def get_snp_category(self, snp_id):
-        pass
+        query = "SELECT category from snp where snp = :snp_id"
+        return self.db.execute(query, { 'snp_id': snp_id }).fetchone()[0]
 
     def test_categorizing_normal_snps(self):
         # Setup
         self.create_snp('normal1')
-        self.create_snp_call('normal1', sample='TLE66_N', genotype='1/1')
+        self.create_snp('normal2')
+        self.create_snp('normal3')
+        self.create_snp_call('normal1', sample='TLE66_N', genotype_simple=2)
+        self.create_snp_call('normal2', sample='TLE66_N', genotype_simple=1)
+        self.create_snp_call('normal3', sample='TLE66_N', genotype_simple=0)
 
         # Execution
-        categorize_snps()
+        categorize_snps(self.db)
 
         # Verification
-        category = self.get_snp_category('normal1')
-        self.assertEqual(category, 'Normal')
+        self.assertEqual(self.get_snp_category('normal1'), 'Normal')
+        self.assertEqual(self.get_snp_category('normal2'), 'Normal')
+        self.assertEqual(self.get_snp_category('normal3'), None)
+
+    def test_categorizing_tumor_snps(self):
+        # Setup
+        self.create_snp('tumor1')
+        self.create_snp('tumor2')
+        self.create_snp('tumor3')
+        self.create_snp_call('tumor1', sample='TLE66_T', genotype_simple=2)
+        self.create_snp_call('tumor2', sample='TLE66_T', genotype_simple=1)
+        self.create_snp_call('tumor3', sample='TLE66_T', genotype_simple=0)
+
+        # Execution
+        categorize_snps(self.db)
+
+        # Verification
+        self.assertEqual(self.get_snp_category('tumor1'), 'Tumor')
+        self.assertEqual(self.get_snp_category('tumor2'), 'Tumor')
+        self.assertEqual(self.get_snp_category('tumor3'), None)
+
+    def test_categorizing_mixed_snps(self):
+        # Setup
+        self.create_snp('normal1')
+        self.create_snp('tumor1')
+        self.create_snp('mixed1')
+
+        self.create_snp('unknown1')
+        self.create_snp('unknown2')
+
+        self.create_snp_call('normal1', sample='TLE66_N', genotype_simple=2)
+        self.create_snp_call('tumor1', sample='TLE66_T', genotype_simple=2)
+        self.create_snp_call('mixed1', sample='TLE66_N', genotype_simple=2)
+        self.create_snp_call('mixed1', sample='TLE66_T', genotype_simple=2)
+
+        self.create_snp_call('unknown1', sample='OTHER', genotype_simple=2)
+        # SNP 'unknown2': No call
+
+        # Execution
+        categorize_snps(self.db)
+
+        # Verification
+        self.assertEqual(self.get_snp_category('normal1'), 'Normal')
+        self.assertEqual(self.get_snp_category('tumor1'), 'Tumor')
+        self.assertEqual(self.get_snp_category('mixed1'), 'Mixed')
+        self.assertEqual(self.get_snp_category('unknown1'), None)
+        self.assertEqual(self.get_snp_category('unknown2'), None)
 
 
 if __name__ == '__main__':
