124ca6fe8bd90c75ba07d56c843ecba752612a30
Andrew Radev <andrey.radev@gmail.com>
2024-07-22 22:56:41 +0200

    Overlap graph step 2: Green inner tests for read overlaps

--- 03_overlap/overlap_graph.py
+++ 03_overlap/overlap_graph.py
@@ -1,10 +1,41 @@
 class OverlapGraph():
     def __init__(self, reads):
+        self.reads = reads
         self.sequence = None
 
-    def find_sequence(self):
-        # 1. loop over all reads, find overlaps
-        # 2. find the root node
-        # 3. walk the graph, building up the sequence
+        self.overlaps = {}
 
+    def find_sequence(self):
+        self.find_read_overlaps()
+        self.find_root_node()
+        self.build_sequence()
+
+    def find_read_overlaps(self):
+        for first in self.reads:
+            for second in self.reads:
+                if first == second: continue
+
+                if first not in self.overlaps:
+                    self.overlaps[first] = []
+
+                overlap = find_overlap_between(first, second)
+                if overlap > 0:
+                    self.overlaps[first].append((overlap, second))
+
+    def find_root_node(self):
         pass
+
+    def build_sequence(self):
+        pass
+
+def find_overlap_between(first, second):
+    max_length = max((len(first), len(second)))
+
+    for i in range(0, max_length):
+        first_tail = first[i:]
+        second_head = second[:len(first_tail)]
+
+        if first_tail == second_head:
+            return len(first) - i
+
+    return 0
--- 03_overlap/test_overlap_graph.py
+++ 03_overlap/test_overlap_graph.py
@@ -1,15 +1,44 @@
 import unittest
 
-from overlap_graph import OverlapGraph
+from overlap_graph import OverlapGraph, find_overlap_between
 
 class TestOverlapGraph(unittest.TestCase):
-    def test_overlap_graph_produces_correct_sequence(self):
+    def test_finding_overlap_between_two_sequences(self):
+        self.assertEqual(find_overlap_between('ABC', 'ABC'), 3)
+        self.assertEqual(find_overlap_between('ABC', 'BCD'), 2)
+        self.assertEqual(find_overlap_between('ABC', 'CDE'), 1)
+        self.assertEqual(find_overlap_between('ABC', 'DEF'), 0)
+
+        self.assertEqual(find_overlap_between('ABCDEF', 'EFG'), 2)
+        self.assertEqual(find_overlap_between('ABC', 'CDEFG'), 1)
+
+        self.assertEqual(find_overlap_between('', ''), 0)
+
+    def test_finding_overlaps_between_a_group_of_sequences(self):
         graph = OverlapGraph(['ABCD', 'CDEF'])
-        graph.find_sequence()
+        graph.find_read_overlaps()
 
-        self.assertEqual(graph.sequence, 'ABCDEF')
+        self.assertEqual(graph.overlaps, {
+            'ABCD': [(2, 'CDEF')],
+            'CDEF': []
+        })
 
-        graph = OverlapGraph(['CDEFG', 'ABCD', 'EFG', 'BC'])
-        graph.find_sequence()
+        graph = OverlapGraph(['ABCDEF', 'EF', 'FGHA'])
+        graph.find_read_overlaps()
 
-        self.assertEqual(graph.sequence, 'ABCDEFG')
+        self.assertEqual(graph.overlaps, {
+            'ABCDEF': [(2, 'EF'), (1, 'FGHA')],
+            'EF': [(1, 'FGHA')],
+            'FGHA': [(1, 'ABCDEF')]
+        })
+
+    # def test_overlap_graph_produces_correct_sequence(self):
+    #     graph = OverlapGraph(['ABCD', 'CDEF'])
+    #     graph.find_sequence()
+    #
+    #     self.assertEqual(graph.sequence, 'ABCDEF')
+    #
+    #     graph = OverlapGraph(['CDEFG', 'ABCD', 'EFG', 'BC'])
+    #     graph.find_sequence()
+    #
+    #     self.assertEqual(graph.sequence, 'ABCDEFG')
